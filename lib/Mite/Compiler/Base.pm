c5bfea4552515787417b709584839b62681f7ce2
Michael G. Schwern <schwern@pobox.com>
2011-12-08 19:42:40 -0800

    This is enough to get a class with a constructor.

---
+++ lib/Mite/Compiler/Base.pm
@@ -0,0 +1,102 @@
+package Mite::Compiler::Base;
+
+use strict;
+use warnings;
+
+# Do the eval in the caller's package and before any other pragma
+# are loaded which might interfere.
+# We do want strict and warnings.
+sub clean_eval {
+    my $self = shift;
+    my $code_ref = shift;
+
+    # Isolate globals
+    local($!, $?, $@);
+
+    my $package = $self->package;
+    my $ok = eval "package $package;\n" . $$code_ref . "; 1";
+    die $@ unless $ok;
+
+    return;
+}
+
+
+sub save_code {
+    my $self = shift;
+    my $code_ref = shift;
+
+    $DB::single = 1;
+
+    my $fh = $self->mite_fh;
+
+    # Save the code in the mite file
+    print $fh $$code_ref;
+
+    # So the file returns a true value
+    print $fh ";1;\n";
+
+    # Load it into the current process
+    $self->clean_eval($code_ref);
+
+    return;
+}
+
+
+sub new {
+    my $class = shift;
+    my %args = @_;
+
+    return bless \%args, $class;
+}
+
+
+sub package {
+    my $self = shift;
+
+    return $self->{package};
+}
+
+sub mite_file {
+    my $self = shift;
+
+    return $self->{mite_file};
+}
+
+# This is not multiprocess safe.
+sub mite_fh {
+    my $self = shift;
+
+    my $mite_file = $self->mite_file;
+
+    # No file exists, make a new one and write the header.
+    if( !-e $mite_file ) {
+        $self->touch($mite_file);
+
+        my $package = $self->package;
+
+        my $code = <<"END";
+package $package;
+
+use strict;
+use warnings;
+
+END
+
+        $self->save_code(\$code);
+    }
+
+    open my $fh, ">>", $self->mite_file;
+
+    return $fh;
+}
+
+
+sub touch {
+    my $self = shift;
+    my $file = shift;
+
+    open my $fh, ">>", $file;
+    close $fh;
+}
+
+1;
---
+++ lib/Mite/Shim.pm
@@ -0,0 +1,89 @@
+package Mite::Shim;
+
+use strict;
+use warnings;
+
+
+=head1 NAME
+
+Mite::Shim
+
+=head1 SYNOPSIS
+
+    # First, make your own copy of the shim
+    mite_shim("Foo::Mite") > lib/Foo/Mite.pm
+
+    # Then use it.
+    package Foo;
+    use Foo::Mite;
+
+=head1 DESCRIPTION
+
+This is a small module which decides if your L<Mite> code needs to be
+recompiled.  It lets your code work without distributing Mite along
+with it.
+
+=cut
+
+sub import {
+    my $class = shift;
+    my($caller, $file) = caller;
+
+    # Turn on warnings and strict in the caller
+    warnings->import;
+    strict->import;
+
+    my $mite_file = _mite_file(
+        package         => $caller,
+        filename        => $file
+    );
+
+    # Rebuild only if the original file is newer or at the same time.
+    # (It's better to build twice than to miss a build)
+    my $do_rebuild = (!-e $mite_file) || ((stat($file))[9] >= (stat($mite_file))[9]);
+
+    if( $do_rebuild and eval { require Mite::Compiler } ) {
+        Mite::Compiler->inject_mite_functions(
+            package     => $caller,
+            mite_file   => $mite_file,
+        );
+    }
+    else {
+        # Load the Mite code
+        require $mite_file;
+
+        # Inject blank Mite routines
+        for my $name (qw( extends has class_has )) {
+            no strict 'refs';
+            *{ $caller .'::'. $name } = sub {};
+        }
+    }
+}
+
+
+sub _mite_file {
+    my %args = @_;
+    my $caller      = $args{package};
+    my $caller_file = $args{filename};
+
+    my $replace = '__'.$caller.'__mite.pm';
+    (my $mite_file = $caller_file) =~ s{$}{$replace};
+
+    return $mite_file;
+}
+
+
+sub clear_code {
+    my($package, $file) = caller;
+
+    my $mite_file = _mite_file(
+        package         => $package,
+        filename        => $file
+    );
+
+    1 while unlink $mite_file;
+
+    return;
+}
+
+1;
