d087725d1e5ef6dd3f27007c4324e47ff524ee00
Michael G. Schwern <schwern@pobox.com>
2014-03-13 18:46:49 -0700

    Split the Mite compiler into its own distribution, Mite::Compiler.
    This allows people to depend on Mite if they don't want to go
    through the mite shim stuff or don't care about the zero
    dependency part.

---
+++ Mite-Compiler/lib/Mite/Compiler.pm
@@ -0,0 +1,20 @@
+package Mite::Compiler;
+
+use version 0.77; our $VERSION = qv("v0.0.1");
+
+=head1 NAME
+
+Mite::Compiler - What you need to develop with Mite
+
+=head1 DESCRIPTION
+
+To come.
+
+=head1 SEE ALSO
+
+L<Mite>, L<mite>
+
+=cut
+
+1;
+
