5b47799661d25d81749333d016ad2a9139397661
Michael G. Schwern <schwern@pobox.com>
2014-03-21 15:22:19 -0700

    Make it so Test::Mite exports mite_compile and mite_load
    Turns out the best way to do this with Fennec is to put all the
    test functions into their own module and load that using
    Fennec's "utils" argument.
    
    mite_compile doesn't work, but it was broken anyway.  Fix coming next.

--- Mite-Compiler/t/lib/Test/Mite.pm
+++ Mite-Compiler/t/lib/Test/Mite.pm
@@ -1,63 +1,91 @@
-package Test::Mite;
+{
+    package Test::Mite;
 
-use v5.10;
-use strict;
-use warnings;
+    use v5.10;
+    use strict;
+    use warnings;
 
-use parent 'Fennec';
+    use parent 'Fennec';
+    use Method::Signatures;
 
-use autodie;
-use Path::Tiny;
-use Method::Signatures;
+    # Export our extra mite testing functions.
+    method defaults($class: ...) {
+        my %params = $class->SUPER::defaults;
 
-our @EXPORT = qw(mite_compile mite_load);
+        push @{ $params{utils} }, "Test::Mite::Functions";
 
-method after_import($class: $info) {
-    # Test the pure Perl implementation.
-    $info->{layer}->add_case(
-        [$info->{importer}, __FILE__, __LINE__ + 1],
-        case_pure_perl => func(...) {
-            $ENV{MITE_PURE_PERL} = 1;
-        }
-    );
-
-    # Test with Class::XSAccessor, if available.
-    $info->{layer}->add_case(
-        [$info->{importer}, __FILE__, __LINE__ + 1],
-        case_xs => func(...) {
-            $ENV{MITE_PURE_PERL} = 0;
-        }
-    ) if eval { require Class::XSAccessor };
-}
-
-func class2pm(Str $class) {
-    my $pm = $class.'.pm';
-    $pm =~ s{::}{/};
-
-    return $pm;
-}
-
-func mite_compile(Str $code, Str :$class, :$dir=tempdir()) {
-    # Write the code to a temp file
-    my $pmfile = $dir->child( class2pm($class) );
-    $pmfile->spew($code);
-
-    # Compile the code
-    {
-        # Do it in its own process to avoid polluting the test process
-        # with compiler code.  This better emulates how it works in production.
-        local $ENV{MITE_COMPILE} = 1;
-        system("$^X $pmfile");
+        return %params;
     }
 
-    return $pmfile;
+    # Test with and without Class::XSAccessor.
+    method after_import($class: $info) {
+        # Test the pure Perl implementation.
+        $info->{layer}->add_case(
+            [$info->{importer}, __FILE__, __LINE__ + 1],
+            case_pure_perl => func(...) {
+                $ENV{MITE_PURE_PERL} = 1;
+            }
+        );
+
+        # Test with Class::XSAccessor, if available.
+        $info->{layer}->add_case(
+            [$info->{importer}, __FILE__, __LINE__ + 1],
+            case_xs => func(...) {
+                $ENV{MITE_PURE_PERL} = 0;
+            }
+        ) if eval { require Class::XSAccessor };
+    }
 }
 
-func mite_load(Str $code) {
-    my $file = mite_compile($code);
 
-    # Allow the same file to be recompiled and reloaded
-    return do $file;
+# Because of the way Fennec works, it's easier to put
+# all our extra functions into their on class.
+{
+    package Test::Mite::Functions;
+
+    use v5.10;
+    use strict;
+    use warnings;
+
+    use parent 'Exporter';
+    our @EXPORT = qw(mite_compile mite_load);
+
+    use Method::Signatures;
+    use Path::Tiny;
+    use Child;
+
+    func _class2pm(Str $class) {
+        my $pm = $class.'.pm';
+        $pm =~ s{::}{/};
+
+        return $pm;
+    }
+
+    func mite_compile(Str $code, Str :$class, :$dir=Path::Tiny->tempdir) {
+        # Write the code to a temp file
+        my $pmfile = $dir->child( _class2pm($class) );
+        $pmfile->spew($code);
+
+        # Compile the code
+        # Do it in its own process to avoid polluting the test process
+        # with compiler code.  This better emulates how it works in production.
+        {
+            local $ENV{MITE_COMPILE} = 1;
+            system("$^X $pmfile");
+        }
+
+        return $pmfile;
+    }
+
+    func mite_load(Str $code) {
+        my $file = mite_compile($code);
+
+        # Allow the same file to be recompiled and reloaded
+        return do $file;
+    }
+
+    # We're loaded, really!
+    $INC{"Test/Mite/Functions.pm"} = __FILE__;
 }
 
 1;
