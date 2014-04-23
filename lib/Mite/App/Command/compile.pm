1f012fe57fde78e9e55e16615a513d8f4ab1bff5
Michael G. Schwern <schwern@pobox.com>
2014-04-23 13:57:18 -0700

    Make --search-mite-dir and --exit-if-no-mite-dir global options.
    Also remove the work around for the MouseX::Foreign bug with App::Cmd.
    They fixed it in 1.000.

--- lib/Mite/App/Command/compile.pm
+++ lib/Mite/App/Command/compile.pm
@@ -13,14 +13,6 @@ method abstract() {
     return "Make your code ready to run";
 }
 
-method opt_spec(...) {
-    return(
-      [ "search-mite-dir!" => "only look for .mite/ in the current directory",
-        { default => 1 } ],
-      [ "exit-if-no-mite-dir!" => "exit quietly if a .mite dir cannot be found",
-        { default => 0 } ],
-    );
-}
 
 method execute($opts, $args) {
     return if $self->should_exit_quietly($opts);
@@ -47,14 +39,6 @@ method project() {
 
 method config() {
     return $self->project->config;
-}
-
-method should_exit_quietly($opts) {
-    my $config = $self->config;
-
-    return unless $opts->{exit_if_no_mite_dir};
-    return 1 if !$opts->{search_mite_dir} && !$config->dir_has_mite(".");
-    return 1 if !$config->find_mite_dir;
 }
 
 1;
